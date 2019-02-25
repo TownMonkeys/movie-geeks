968235c4bc25e3dbbc1101aab7a0629519002dd4
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-02-25 22:38:31 +0200

    finished header (signed in + desktop)

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,13 +1,17 @@
 import React from 'react';
+import user from '../../images/user.jpg';
 
 const NavBar = () => {
   return (
     <nav className="navigation header__navigation" role="navigation">
       <h2 className="navigation__heading">Navigation Bar</h2>
 
-      <ul className="navigationMenu">
+      <ul className="navigationMenu navigation__navigationMenu">
         <li className="navigationMenu__item">
-          <a href="#" className="navigationMenu__link">Profile</a>
+          <a href="#" className="navigationMenu__link">
+            <img className="avatar navigationMenu__avatar" src={user} alt="User Photo"/>
+            <span>Moaaz</span>
+          </a>
         </li>
         <li className="navigationMenu__item">
           <a href="#" className="navigationMenu__link">Add Movie</a>
--- src/components/layout/Notifications.js
+++ src/components/layout/Notifications.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import notifications from '../../images/notifications.svg';
 
 const Notifications = () => {
   return (
-    <button className="button header__button" type="button">Notifications</button>
+    <button className="button notifications" type="button">
+      <img className="image" src={notifications} alt="Notifications Icon"/>
+      <span className="notifications__badge">3</span>
+    </button>
   );
 }
 
