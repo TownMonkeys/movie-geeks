fa8d4e0689921fb875debde58b1d675540b06799
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-09 23:36:42 +0200

    like on heart click or image double click

--- src/components/movie/LikeSvg.js
+++ src/components/movie/LikeSvg.js
@@ -1,15 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import './LikeSvg.scss';
 
-const LikeSvg = () => {
+const LikeSvg = (props) => {
+  const {clicked} = props;
+
   return (
     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
     viewBox="0 0 50 50" style={{enableBackground: 'new 0 0 50 50'}} xmlSpace="preserve">
-      <path className="movie__heartPath" fill="#D75A4A" d="M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543
+      <path className={`movie__heartPath ${clicked ? 'movie__heartPath_filled' : 'movie__heartPath_empty'}`} fill="#D75A4A" d="M24.85,10.126c2.018-4.783,6.628-8.125,11.99-8.125c7.223,0,12.425,6.179,13.079,13.543
       c0,0,0.353,1.828-0.424,5.119c-1.058,4.482-3.545,8.464-6.898,11.503L24.85,48L7.402,32.165c-3.353-3.038-5.84-7.021-6.898-11.503
       c-0.777-3.291-0.424-5.119-0.424-5.119C0.734,8.179,5.936,2,13.159,2C18.522,2,22.832,5.343,24.85,10.126z"/>
     </svg>
   );
+}
+
+LikeSvg.propTypes = {
+  clicked: PropTypes.bool
 }
 
 export default LikeSvg;
--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import hxh from '../../images/hunterxhunter.jpg';
 import './Movie.scss';
 import LikeSvg from './LikeSvg';
@@ -6,45 +6,67 @@ import user from '../../images/user.svg';
 import star from '../../images/star.svg';
 import StarSvg from './StarSvg';
 
-const Movie = () => {
-  return (
-    <article className="movie">
-      <header className="movie__header">
-        <a href="#" className="movie__userAvatarLink">
-          <img src={user} alt="Hajar's avatar" className="image avatar avatar_size_med"/>
-        </a>
-        <a href="#" className="movie__userNameLink">Hajar Bhnas</a>
-      </header> {/* Header */}
+class Movie extends Component {
+  state = { 
+    likeClicked: false
+  }
 
-      <button className="button movie__imageButton" type="button">
-        <img src={hxh} className="image movie__image"/>
-      </button> {/* Image */}
-      <h4 className="movie__name">Hunter x Hunter</h4> {/* Name */}
-      <span className="movie__genre">Animation, Adventure</span> {/* Genre */}
-      <ul className="list movie__starList">
-      {
-        Array(5).fill(null).map(e => (
-          <li className="movie__starItem">
-            <StarSvg />
-          </li>
-        ))
-      }
-      </ul>
-      <p className="movie__review">
-        Fugit enim veniam mollitia ea illum voluptatem fuga aperiam. Esse dolorem nobis autem fuga non.
-      </p> {/* Review */}
-      <button className="button movie__likeButton">
-        <LikeSvg />
-      </button> {/* Like */}
-      <p className="movie__likeNumber">
-        <a className="movie__liker" href="#">Mohamed</a> and <a className="movie__otherLikers" href="#">5 others</a> liked your post
-      </p> {/* Likes Number */}
+  hitLike = () => {
+    this.setState({
+      likeClicked: true
+    });
+  }
 
-      <footer className="movie__footer">
-        <time className="movie__time">28 minutes ago</time>
-      </footer>
-    </article>
-  );
+  toggleLike = () => {
+    this.setState((prev) => ({
+      likeClicked: !prev.likeClicked
+    }));
+  }
+
+  render() {
+    return (
+      <article className="movie">
+        <header className="movie__header">
+          <a href="#" className="movie__userAvatarLink">
+            <img src={user} alt="Hajar's avatar" className="image avatar avatar_size_med"/>
+          </a>
+          <a href="#" className="movie__userNameLink">Hajar Bhnas</a>
+        </header> {/* Header */}
+
+        <button 
+          className="button movie__imageButton" 
+          type="button"
+          onDoubleClick={this.hitLike}
+        >
+          <img src={hxh} className="image movie__image"/>
+        </button> {/* Image */}
+        <h4 className="movie__name">Hunter x Hunter</h4> {/* Name */}
+        <span className="movie__genre">Animation, Adventure</span> {/* Genre */}
+        <ul className="list movie__starList">
+        {
+          Array(5).fill(null).map(e => (
+            <li className="movie__starItem">
+              <StarSvg />
+            </li>
+          ))
+        }
+        </ul>
+        <p className="movie__review">
+          Fugit enim veniam mollitia ea illum voluptatem fuga aperiam. Esse dolorem nobis autem fuga non.
+        </p> {/* Review */}
+        <button onClick={this.toggleLike} className="button movie__likeButton">
+          <LikeSvg clicked={this.state.likeClicked} />
+        </button> {/* Like */}
+        <p className="movie__likeNumber">
+          <a className="movie__liker" href="#">Mohamed</a> and <a className="movie__otherLikers" href="#">5 others</a> liked your post
+        </p> {/* Likes Number */}
+
+        <footer className="movie__footer">
+          <time className="movie__time">28 minutes ago</time>
+        </footer>
+      </article>
+    );
+  }
 }
 
 export default Movie;
