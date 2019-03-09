13f85b3c62f15d6e98fbeb34e5b25e846a42c5ed
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-03-09 20:12:20 +0200

    finished all header components

--- src/components/movie/Movie.js
+++ src/components/movie/Movie.js
@@ -2,12 +2,21 @@ import React from 'react';
 import hxh from '../../images/hunterxhunter.jpg';
 import './Movie.scss';
 import LikeSvg from './LikeSvg';
+import user from '../../images/user.svg'
 
 const Movie = () => {
   return (
     <React.Fragment>
-      <li className="movie">
-        <article>
+      <li>
+        <article className="movie">
+          <header>
+            <a href="#" className="movie__userAvatarLink">
+              <img src={user} alt="Hajar's avatar" className="image avatar avatar_size_med"/>
+            </a>
+            <a href="#" className="movie__userNameLink">
+              Hajar Bhnas
+            </a>
+          </header>
           <button className="button movie__imageButton" type="button">
             <img src={hxh} className="image movie__image"/>
           </button>{/* Image */}
@@ -18,6 +27,9 @@ const Movie = () => {
             <LikeSvg />
           </button> {/* Like */}
           <p className="movie__likeNumber">Mohamed and 5 others liked your post</p> {/* Likes Number */}
+          <footer>
+            <time>28 minutes ago</time>
+          </footer>
         </article>
       </li>
     </React.Fragment>
