25f38a1d2c13f90fec16902f4913e812dec9a007
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-05-18 17:58:26 +0400

    changed structure to add pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
-import Header from './header/Header';
+import Header from './components/header/Header';
 import './App.scss';
-import './container.scss';
-import './list.scss';
-import './image.scss';
-import './avatar.scss';
-import './button.scss';
-import Dashboard from './dashboard/Dashboard';
+import './sass/basic-components/container.scss';
+import './sass/basic-components/list.scss';
+import './sass/basic-components/image.scss';
+import './sass/basic-components/avatar.scss';
+import './sass/basic-components/button.scss';
+import Dashboard from './components/dashboard/Dashboard';
 
 class App extends Component {
   state = { 
