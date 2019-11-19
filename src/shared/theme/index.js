e279aae247f8a6a761deac1522bc19d4b34cc191
moaaz-bhnas <moaaz_bs@yahoo.com>
2019-11-20 01:07:45 +0200

    Added theme file

---
+++ src/shared/theme/index.js
@@ -0,0 +1,14 @@
+export const theme = {
+  text: {
+    default: '#212121',
+    grey: '#616161'
+  },
+  bg: {
+    default: '#ffffff',
+    grey: '#f5f5f5'
+  },
+  border: {
+    light: '#e0e0e0',
+    dark: '#bdbdbd'
+  }
+}
