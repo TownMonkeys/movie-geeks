491507a2ff8a9277e6ff4bac42e41935ea7ff3c7
MhamedEl-shahawy <mohamedelshahawy599@gmail.com>
2020-03-04 18:54:36 +0200

    listen to user input

--- src/components/reviewForm/index.js
+++ src/components/reviewForm/index.js
@@ -1,4 +1,6 @@
-import React, { memo } from 'react';
+import React, { memo,useState,useRef  } from 'react';
+import axios from 'axios';
+
 import { 
   Form,
   Title,
@@ -7,6 +9,20 @@ import {
 } from './style';
 
 const ReviewForm = () => {
+    const [movies, setMovies] = useState(0);
+    const inputElMovie = useRef(null);
+
+    function moviesFetch(e){
+      e.preventDefault();
+      
+      axios.get(`https://api.themoviedb.org/3/search/multi?query=${inputElMovie.current.value}&api_key=400225a1886f38d9cf3c934d6a756c4d`)
+      .then(res => {
+        const newMovies = res.data;
+        setMovies({ newMovies });
+        console.log(newMovies);
+      })
+    }
+   
   return (
     <Form>
       <Title>Review Movie</Title>
@@ -16,7 +32,10 @@ const ReviewForm = () => {
           type="search"
           aria-label="movie name"
           placeholder="Type the movie name .."
+          ref={inputElMovie}
+          onKeyUp={moviesFetch}
         />
+ 
       </FormBody>
     </Form>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import App from './app';
+import App from './App';
 import GlobalStyles from './global.css';
 import { createStore, applyMiddleware, compose } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
