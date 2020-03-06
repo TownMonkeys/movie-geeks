0123844ed73edeac388365a521b3622adadecb8e
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-03-06 08:57:36 +0200

    spaces and indentations

--- src/components/reviewForm/index.js
+++ src/components/reviewForm/index.js
@@ -1,4 +1,4 @@
-import React, { memo,useState,useRef  } from 'react';
+import React, { memo, useState, useRef  } from 'react';
 import axios from 'axios';
 
 import { 
@@ -9,19 +9,19 @@ import {
 } from './style';
 
 const ReviewForm = () => {
-    const [movies, setMovies] = useState(0);
-    const inputElMovie = useRef(null);
+  const [movies, setMovies] = useState();
+  const inputElMovie = useRef(null);
 
-    function moviesFetch(e){
-      e.preventDefault();
-      
-      axios.get(`https://api.themoviedb.org/3/search/multi?query=${inputElMovie.current.value}&api_key=400225a1886f38d9cf3c934d6a756c4d`)
-      .then(res => {
-        const newMovies = res.data;
-        setMovies({ newMovies });
-        console.log(newMovies);
-      })
-    }
+  function moviesFetch(e){
+    e.preventDefault();
+    
+    axios.get(`https://api.themoviedb.org/3/search/multi?query=${inputElMovie.current.value}&api_key=400225a1886f38d9cf3c934d6a756c4d`)
+    .then(res => {
+      const newMovies = res.data;
+      setMovies({ newMovies });
+      console.log(newMovies);
+    })
+  }
    
   return (
     <Form>
