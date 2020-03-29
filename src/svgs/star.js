c07ba5b74b87853ac333d3a9a6c41d926395e850
moaaz-bhnas <moaaz_bs@yahoo.com>
2020-03-29 22:12:33 +0200

    added a comment

--- src/svgs/star.js
+++ src/svgs/star.js
@@ -6,7 +6,7 @@ import {
 } from './style';
 
 const StarSvg = (props) => {
-  const { fullness } = props;
+  const { fullness } = props; // full, half, or empty
 
   return (
     <StyledStarSvg role="img" aria-labelledby="starSvgTitle" version="1.1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" style={{enableBackground: "new 0 0 24 24"}}>
